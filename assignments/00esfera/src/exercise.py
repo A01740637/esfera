2ddfe33d4496e6d46d806067cdb6874381e90424
casandraortiz <A01740637@itesm.mx>
2021-08-17 14:07:15 +0000

    resuelto del ejercicio de la esfera

---
+++ assignments/00esfera/src/exercise.py
@@ -0,0 +1,17 @@
+import math 
+
+
+def main():
+    #escribe tu código abajo de esta línea
+    print("Este programa calcula el área y el volumen de una esfera")
+
+    radio = float(input("Dame el valor del radio: "))
+
+    area = 4 * math.pi *math.pow(radio,2)
+    volumen = (4/3) * math.pi * math.pow(radio,3)
+
+    print(f"El área de la esfera es: {area}")
+    print(f"El área de la esfera es: {volumen}")
+
+if __name__=='__main__':
+    main()
